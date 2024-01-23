827169a96ba38856c3d9f7c0ef14d701b613ee4f
Jese Ki <2094901072@qq.com>
2024-01-23 22:26:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import json
 from config import Config , load_config , conf
 
-with open('plugins/weaviate_database/config.json', 'r', encoding='utf-8') as f:
+with open('plugins/chatgpt-on-wechat-weaviate-plugin/config.json', 'r', encoding='utf-8') as f:
     config = json.load(f)
 print(config)  # Add this line
 weaviate_url = config['weaviate_url']
